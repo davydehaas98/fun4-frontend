d0d7359fea88a6bf5dc10ff4344096e18b17cea7
davy.dehaas98 <davy.dehaas98@gmail.com>
2019-05-16 23:53:35 +0200

    Change connection string

--- src/variables.js
+++ src/variables.js
@@ -1,2 +1,2 @@
-export const connection = 'http://172.17.0.1:4053/api'
-//export const connection = 'http://davydehaas.nl:4053/api'
+//export const connection = 'http://172.17.0.1:4053/api'
+export const connection = 'http://davydehaas.nl:4053/api'
